82c0417d9c53d6b14c816d1ee598647e8eec9cec
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-04-04 10:45:31 -0500

    initial start

---
+++ AppointmentScheduler/Globals/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Globals
+{
+    public static class Database
+    {
+        public static string Server = "127.0.01";
+        public static int Port = 3306;
+        public static string DatabaseName = "client_scheduler";
+        public static string Username = "sqlUser";
+        public static string Password = "Passw0rd!";
+    }
+}
--- AppointmentScheduler/Program.cs
+++ AppointmentScheduler/Program.cs
@@ -16,7 +16,7 @@ namespace AppointmentScheduler
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
