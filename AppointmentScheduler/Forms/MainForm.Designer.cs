52ff75bc5b5bcb9adc211dc7f6c1a4e28616b2ed
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-04-05 07:14:39 -0500

    Add new UI components to MainForm and update layout
    Updated InitializeComponent in MainForm.Designer.cs to add new UI components: AddBtn, DeleteBtn, ModifyBtn, ReportsBtn, monthCalendar1, LogoutBtn, CloseBtn, and notifyIcon1. Adjusted properties such as location, name, size, tab index, text, and visibility. Changed MainForm ClientSize from 800, 450 to 1055, 636 to fit new components. Added new components to Controls collection. Declared private fields for new components. Added metadata for notifyIcon1 in MainForm.resx.

--- AppointmentScheduler/Forms/MainForm.Designer.cs
+++ AppointmentScheduler/Forms/MainForm.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.ModifyBtn = new System.Windows.Forms.Button();
+            this.ReportsBtn = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.LogoutBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(18, 192);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(113, 23);
+            this.AddBtn.TabIndex = 0;
+            this.AddBtn.Text = "Add Appointment";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(18, 221);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(113, 23);
+            this.DeleteBtn.TabIndex = 1;
+            this.DeleteBtn.Text = "Delete Appointment";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // ModifyBtn
+            // 
+            this.ModifyBtn.Location = new System.Drawing.Point(18, 250);
+            this.ModifyBtn.Name = "ModifyBtn";
+            this.ModifyBtn.Size = new System.Drawing.Size(113, 23);
+            this.ModifyBtn.TabIndex = 2;
+            this.ModifyBtn.Text = "Modify Appointment";
+            this.ModifyBtn.UseVisualStyleBackColor = true;
+            // 
+            // ReportsBtn
+            // 
+            this.ReportsBtn.Location = new System.Drawing.Point(18, 279);
+            this.ReportsBtn.Name = "ReportsBtn";
+            this.ReportsBtn.Size = new System.Drawing.Size(113, 23);
+            this.ReportsBtn.TabIndex = 3;
+            this.ReportsBtn.Text = "Reports";
+            this.ReportsBtn.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            // 
+            // LogoutBtn
+            // 
+            this.LogoutBtn.Location = new System.Drawing.Point(12, 576);
+            this.LogoutBtn.Name = "LogoutBtn";
+            this.LogoutBtn.Size = new System.Drawing.Size(113, 23);
+            this.LogoutBtn.TabIndex = 5;
+            this.LogoutBtn.Text = "Log Out";
+            this.LogoutBtn.UseVisualStyleBackColor = true;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Location = new System.Drawing.Point(12, 605);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(113, 23);
+            this.CloseBtn.TabIndex = 6;
+            this.CloseBtn.Text = "Close";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1055, 636);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.LogoutBtn);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.ReportsBtn);
+            this.Controls.Add(this.ModifyBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.AddBtn);
             this.Name = "MainForm";
             this.Text = "Main";
             this.ResumeLayout(false);
@@ -42,6 +123,15 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button ModifyBtn;
+        private System.Windows.Forms.Button ReportsBtn;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button LogoutBtn;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
