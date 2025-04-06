1bfc15bee1cac599afb1433377f0d42bda34f62c
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-04-06 07:23:01 -0500

    layout reminder form

--- AppointmentScheduler/Forms/Reminder.Designer.cs
+++ AppointmentScheduler/Forms/Reminder.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ReminderTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 304);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ReminderTitle
+            // 
+            this.ReminderTitle.AutoSize = true;
+            this.ReminderTitle.ForeColor = System.Drawing.Color.Red;
+            this.ReminderTitle.Location = new System.Drawing.Point(351, 62);
+            this.ReminderTitle.Name = "ReminderTitle";
+            this.ReminderTitle.Size = new System.Drawing.Size(117, 13);
+            this.ReminderTitle.TabIndex = 1;
+            this.ReminderTitle.Text = "Appointment Reminder!";
+            // 
+            // Reminder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReminderTitle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Reminder";
             this.Text = "Reminder";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label ReminderTitle;
     }
 }
